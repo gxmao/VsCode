39682abdc6a432761f8aff17129df9421d0fd410
gxmao <143677892+gxmao@users.noreply.github.com>
2023-09-21 11:27:57 -0300

    Add files via upload

---
+++ AulaC1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+// standard - input - output
+// std - i - o
+// stdio
+#include <stdlib.h>
+int main()
+{
+    printf("Ta Ligado!");
+    return 0;
+}
+// main = função
+//int = inteiro
